2936da18cabd83bbf4a24b2aa87984e9cb79e9f2
KrisK1978 <k_kempa@yahoo.co.uk>
2020-11-26 20:27:12 +0000

    Connect Flask to Mongo

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def guitars_list():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
