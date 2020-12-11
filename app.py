649da7ad670c581027ef0621701981faa97fcda3
KrisK1978 <k_kempa@yahoo.co.uk>
2020-12-11 08:20:10 +0000

    Added error page function

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# Homepage
 @app.route('/')
 def about():
     """
-    The function loads the about page
+    The function lrenders the homepage
     """
     return render_template('pages/about.html')
 
@@ -30,41 +31,19 @@ def about():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     """
-    Allows new user to register on the webpage
-    It checks if the username already exists in database
-    Redirects the user to guitars page
+    Allows new user to register on the webpage,
+    checks if the username already exists in database
     """
-    if request.method == "POST":
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            flash("Sorry, this username already exists!")
-            return redirect(url_for("register"))
-
-        username = request.form.get("username").lower()
-        password = generate_password_hash(request.form.get("password"))
-
-        mongo.db.users.insert_one({
-            'username': username,
-            'password': password})
- 
-        if mongo.db.users.find_one({'username': username}) is not None:
-            user = mongo.db.users.find_one({'username': username})
-            user_id = user['_id']
-            session['user_id'] = str(user_id)
-            guitars = mongo.db.guitars({"user_id": user_id})
-            return redirect(url_for("blank_form", user_id=user_id))
-
-    return render_template("pages/user_authentication.html", register=True)
+    
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     """
-    Allows alredy registered user to log in 
+    Allows alredy registered user to log in
     """
 
+# Guitars
 @app.route('/guitars')
 def guitars():
     """
@@ -73,6 +52,28 @@ def guitars():
     """
     guitars = mongo.db.guitars.find()
     return render_template('pages/guitars.html', guitars=guitars)
+
+
+# 404 error page
+@app.errorhandler(404)
+def page_not_found():
+    """
+    Renders an error page with 404 message
+    """
+    error_message = str(error)
+    return render_template('pages/error.html',
+                           error_message=error_message), 400
+
+
+# 500 error page
+@app.errorhandler(500)
+def server_error():
+    """
+    Renders an error page with 500 message
+    """
+    error_message = str(error)
+    return render_template('pages/error.html',
+                           error_message=error_message), 500
 
 
 if __name__ == '__main__':
