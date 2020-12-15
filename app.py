7a4b58bb2876c7224dabef2e5d79a65204185c1b
KrisK1978 <k_kempa@yahoo.co.uk>
2020-12-15 14:32:36 +0000

    Restyled homepage button in login component

--- app.py
+++ app.py
@@ -59,8 +59,31 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """
-    Allows alredy registered user to log in
+    Allows alredy registered user to log in,
+    checks if the username already exists in database,
+    ensures hashed password matches user input
     """
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Hello, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Sorry, Your Username and/or Password is incorrect!")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Sorry, Your Username and/or Password is incorrect!")
+            return redirect(url_for("login"))
+
     return render_template("components/forms/login_form.html")
 
 
